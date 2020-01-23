194e80d52efd825b38e1164dcd9b73a4b955bbe8
chithanh1811 <chithanh1811@gmail.com>
2020-01-23 15:27:40 +0100

    Add files via upload

--- test.js
+++ test.js
@@ -4,15 +4,15 @@ $(function () { // wait for document ready
 
     // define movement of panels
     var wipeAnimation = new TimelineMax()
-        .fromTo(".section.two",    1, {y:  "100%"}, {y: "0%", ease: Linear.easeNone})  // in from right
-        .fromTo(".section.three", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone}) // in from top
-        .fromTo(".section.four", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
+        .fromTo(".section.two",    1, {y:  "100%"}, {y: "0", ease: Linear.easeNone})  // in from right
+        .fromTo(".section.three", 1, {y: "100%"}, {y: "0", ease: Linear.easeNone}) // in from top
+        .fromTo(".section.four", 1, {y: "100%"}, {y: "0", ease: Linear.easeNone}); // in from top
 
     // create scene to pin and link animation
     new ScrollMagic.Scene({
             triggerElement: ".sections",
             triggerHook: "onLeave",
-            duration: "300%"
+			duration: "7000vh"
         })
         .setPin(".sections")
         .setTween(wipeAnimation)
@@ -22,37 +22,90 @@ $(function () { // wait for document ready
     var controller = new ScrollMagic.Controller();
     new ScrollMagic.Scene({
 		triggerElement: ".sections",
-		duration: "10%",
-		offset: "1500%"
+		duration: "300vh",
+		offset: "3000vh"
 	})
 	.setTween(new TweenMax.to("#overall95", 1, {css: {opacity: 0}, ease: Linear.easeNone}))
-	.addIndicators({name: "95"})
+	.addIndicators()
 	.addTo(controller);
 
 	new ScrollMagic.Scene({
 		triggerElement: ".sections",
-		duration: "10%",
-		offset: "1700%"
+		duration: "300vh",
+		offset: "3000vh"
+	})
+	.setTween(new TweenMax.to("#overall95d", 1, {css: {opacity: 0}, ease: Linear.easeNone}))
+	.addIndicators()
+	.addTo(controller);
+
+	new ScrollMagic.Scene({
+		triggerElement: ".sections",
+		duration: "300vh",
+		offset: "3000vh"
+	})
+	.setTween(new TweenMax.to("#overallxpd", 1, {css: {opacity: 100}, ease: Linear.easeNone}))
+	.addIndicators()
+	.addTo(controller);
+	
+	new ScrollMagic.Scene({
+		triggerElement: ".sections",
+		duration: "300vh",
+		offset: "3500vh"
+	})
+	.setTween(new TweenMax.to("#overall95d", 1, {css: {opacity: 0}, ease: Linear.easeNone}))
+	.addIndicators()
+	.addTo(controller);
+
+	new ScrollMagic.Scene({
+		triggerElement: ".sections",
+		duration: "300vh",
+		offset: "3500vh"
+	})
+	.setTween(new TweenMax.to("#overallxpd", 1, {css: {opacity: 0}, ease: Linear.easeNone}))
+	.addIndicators()
+	.addTo(controller);
+	new ScrollMagic.Scene({
+		triggerElement: ".sections",
+		duration: "300vh",
+		offset: "4000vh"
 	})
 	.setTween(new TweenMax.to("#overallxp", 1, {css: {opacity: 0}, ease: Linear.easeNone}))
-	.addIndicators({name: "XP"})
+	.addIndicators()
+	.addTo(controller);
+	
+	new ScrollMagic.Scene({
+		triggerElement: ".sections",
+		duration: "300vh",
+		offset: "4000vh"
+	})
+	.setTween(new TweenMax.to("#overallvistad", 1, {css: {opacity: 0}, ease: Linear.easeNone}))
+	.addIndicators()
 	.addTo(controller);
 
 	new ScrollMagic.Scene({
 		triggerElement: ".sections",
-		duration: "10%",
-		offset: "1900%"
+		duration: "300vh",
+		offset: "3500vh"
+	})
+	.setTween(new TweenMax.to("#overallvistad", 1, {css: {opacity: 100}, ease: Linear.easeNone}))
+	.addIndicators()
+	.addTo(controller);
+
+	new ScrollMagic.Scene({
+		triggerElement: ".sections",
+		duration: "300vh",
+		offset: "4000vh"
 	})
 	.setTween(new TweenMax.to("#overallvista", 1, {css: {opacity: 0}, ease: Linear.easeNone}))
-	.addIndicators({name: "Vista"})
+	.addIndicators()
 	.addTo(controller);
 
 	new ScrollMagic.Scene({
 		triggerElement: ".sections",
-		duration: "10%",
-		offset: "2100%"
+		duration: "300vh",
+		offset: "4000vh"
 	})
-	.setTween(new TweenMax.to("#overallw10", 1, {css: {opacity: 0}, ease: Linear.easeNone}))
-	.addIndicators({name: "10"})
+	.setTween(new TweenMax.to("#overallw10d", 1, {css: {opacity: 100}, ease: Linear.easeNone}))
+	.addIndicators()
 	.addTo(controller);
 });
